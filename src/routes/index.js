dc5562a854f839f075b7d0c62f168311becbbbf6
lekoundanmgedeon <lekounda.n.m.gedeon@aims-senegal.org>
2026-05-25 01:07:39 +0000

    add linkactivate attribute

--- src/routes/index.js
+++ src/routes/index.js
@@ -32,6 +32,8 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  linkActiveClass: 'active',       
+  linkExactActiveClass: 'active',
 });
 
 export default router;
